96c7ca0491351eb6428246bc9ae9f70ed8da722e
GeorgeEsbergen <123629554+GeorgeEsbergen@users.noreply.github.com>
2024-04-21 02:33:56 +0200

    Screenshot created and saved to mobile

--- lib/features/buttom_nav_bar/profile/report/report.dart
+++ lib/features/buttom_nav_bar/profile/report/report.dart
@@ -10,7 +10,7 @@ class Report extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold (
+    return  const Scaffold (
       body: ReportDetails(),
     );
   }
--- lib/features/buttom_nav_bar/profile/report/report_datils.dart
+++ lib/features/buttom_nav_bar/profile/report/report_datils.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../../../core/widgets/buttons.dart';
@@ -6,15 +8,44 @@ import '../../../../model/report_chart/cahrt_model.dart';
 import '../../../homepage/widgets/conatainers.dart';
 import 'widgets/cards.dart';
 import 'widgets/line_chart.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 
-class ReportDetails extends StatelessWidget {
+class ReportDetails extends StatefulWidget {
   const ReportDetails({super.key});
+
+  @override
+  State<ReportDetails> createState() => _ReportDetailsState();
+}
+
+class _ReportDetailsState extends State<ReportDetails> {
+  ScreenshotController screenshotController = ScreenshotController();
+
+  // ignore: non_constant_identifier_names
+  SaveToGallery() {
+    screenshotController.capture().then((Uint8List? image) {
+      saveScreenshot(image!);
+    });
+    ScaffoldMessenger.of(context)
+        .showSnackBar(const SnackBar(content: Text("Image Saved to gallery")));
+  }
+
+  saveScreenshot(Uint8List byte) async {
+    final time = DateTime.now()
+        .toIso8601String()
+        .replaceAll('.', '-')
+        .replaceAll(':', '-');
+    final name = "Screenshot$time";
+    await ImageGallerySaver.saveImage(byte, name: name);
+  }
+
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return SingleChildScrollView(
       child: SafeArea(
           child: Container(
+        color: Colors.white,
         margin: const EdgeInsets.only(right: 20, left: 15, top: 20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -22,25 +53,40 @@ class ReportDetails extends StatelessWidget {
             const MaianAppBar(
               text: 'Report',
             ),
-            const SizedBox(height: 40),
-            SizedBox(
-              width: double.infinity,
-              height: 350,
-              child: LineChartW(glData),
+            const SizedBox(height: 20),
+            Screenshot(
+              controller: screenshotController,
+              child: Container(
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 350,
+                      child: LineChartW(glData),
+                    ),
+                    const SizedBox(height: 30),
+                    Center(child: NextMedican(size: size)),
+                    const SizedBox(height: 30),
+                    const Rowl(),
+                    const SizedBox(height: 70),
+                  ],
+                ),
+              ),
             ),
-            const SizedBox(height: 30),
-            Center(child: NextMedican(size: size)),
-            const SizedBox(height: 30),
-            const Rowl(),
-            BlueButton(fn: () {},  buttonName: 'Save')
+            Center(
+                child: BlueButton(
+                    fn: () {
+                      SaveToGallery();
+                    },
+                    buttonName: 'Save')),
+            const SizedBox(height: 100),
           ],
         ),
       )),
     );
   }
-  
-
-  
 }
 
 class Rowl extends StatelessWidget {
@@ -65,13 +111,7 @@ class Rowl extends StatelessWidget {
           unit: 'lbs',
           title: 'Weight',
         ),
-        
       ],
-    
     );
   }
 }
-
-
-
-
